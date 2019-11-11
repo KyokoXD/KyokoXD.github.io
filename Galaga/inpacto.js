78e1e33add70f310015cd9794d59a58ff913f352
KyokoXD <48030617+KyokoXD@users.noreply.github.com>
2019-11-11 14:38:18 -0300

    Update inpacto.js

--- Galaga/inpacto.js
+++ Galaga/inpacto.js
@@ -2,16 +2,29 @@ let atropelaPlayer = false;
 
 function inpactoPlayer(){
   
-  //Teste carro a carro
+  //Teste das pedras
   for(let i=0; i<imgPedras.length; i++){
     
     atropelaPlayer = collideRectRect(xPlayer, yPlayer, 28, 28, xPedras[i], yPedras[i], 50,30);
     
     if(atropelaPlayer){
-      //Vaca atropelada
+      //Nave atropelada
       yPlayer = 367;
       if(lifePlayer > 0)//Evita placar negativo
         lifePlayer -= 1;
     }
+  }//For
+}//atropelamento
+
+function inpactoBoss(){
+    
+    atropelaBoss = collideRectRect(xPlayer, yPlayer, 28, 28, xBoss, yBoss, 0,0);
+  
+    if(atropelaBoss){
+      //Nave atropelada
+      yBoss = 100;
+      if(lifeBoss > 0)//Evita placar negativo
+        lifeBoss -= 1;
+    
   }//For
 }//atropelamento
